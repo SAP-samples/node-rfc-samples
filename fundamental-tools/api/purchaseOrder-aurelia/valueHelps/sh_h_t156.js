e92479ed66f7d3296f580635f80b1dae49dbec59
bsrdjan <srdjan.boskovic@sap.com>
2021-04-14 18:37:43 +0200

    Purchase Order: Aurelia, Angular, React

---
+++ fundamental-tools/api/purchaseOrder-aurelia/valueHelps/sh_h_t156.js
@@ -0,0 +1,33 @@
+// SH H_T156 Movement type (inventory management) : abap 2.2.0 at: 2021-04-14 18:32:56
+
+const helpSign = [{ id: 'I', name: 'Include' }, { id: 'E', name: 'Exclude' }];
+const helpOption = [
+  { id: 'EQ', name: 'is' },
+  { id: 'NE', name: 'is not' },
+  { id: 'GT', name: 'greater than' },
+  { id: 'LT', name: 'less than' },
+  { id: 'GE', name: 'not less' },
+  { id: 'LE', name: 'not greater' },
+  { id: 'BT', name: 'between' },
+  { id: 'NB', name: 'not between' },
+  { id: 'CP', name: 'with pattern' },
+  { id: 'NP', name: 'w/o pattern' }
+];
+
+const title = "Movement type (inventory management)";
+
+const selectParams = [
+  {
+    FIELDNAME: "BWART",
+    DATATYPE: "CHAR",
+    LENG: 3,
+    DECIMALS: 0,
+    MEMORYID: "BWA"
+  },
+  {
+    FIELDNAME: "BTEXT",
+    DATATYPE: "CHAR",
+    LENG: 20,
+    DECIMALS: 0,
+  }
+];
