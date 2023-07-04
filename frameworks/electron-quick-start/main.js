943ef7a1e8651624a4b7a56e5a004383f4ecb2b7
unknown <srdjan.boskovic@sap.com>
2023-07-04 13:26:36 +0200

    electron integration fix

--- frameworks/electron-quick-start/main.js
+++ frameworks/electron-quick-start/main.js
@@ -12,6 +12,8 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
       preload: path.join(__dirname, "preload.js"),
     },
   });
--- frameworks/electron-quick-start/preload.js
+++ frameworks/electron-quick-start/preload.js
@@ -5,6 +5,7 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 const Client = require("node-rfc").Client;
+
 window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector);
