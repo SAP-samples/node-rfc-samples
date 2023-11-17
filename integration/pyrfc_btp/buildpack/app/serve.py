0cbeed4b6824aee617fc45bad37048436b1e452b
bsrdjan <srdjan.boskovic@sap.com>
2023-11-17 11:09:35 +0100

    refactor(integration): pyrfc btp connectivity

--- integration/pyrfc_btp/buildpack/app/serve.py
+++ integration/pyrfc_btp/buildpack/app/serve.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from pyrfc import Connection
 from pyrfc import __version__ as pyrfc_version
 
@@ -8,14 +8,15 @@ app = Flask(__name__)
 port = int(os.environ.get("PORT", 3000))
 
 
-@app.route("/user/<user_id>")
-def get_user(user_id):
+@app.route("/dest/<dest_id>")
+def get_dest(dest_id="MME"):
     try:
-        client = Connection(dest="MME")
-        result = client.call("BAPI_USER_GET_DETAIL", USERNAME=user_id)
-        return result
-    except Exception:
-        return
+        client = Connection(dest=dest_id)
+        result = client.call("BAPI_USER_GET_DETAIL", USERNAME="MME-TECH-02")
+        return jsonify(result)
+    except Exception as ex:
+        print(ex)
+        return jsonify({"error": str(ex)})
 
 
 @app.route("/")
@@ -24,4 +25,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", debug=True, port=port)
